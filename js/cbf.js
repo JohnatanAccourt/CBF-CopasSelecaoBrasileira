a24a0debe19d3b21d0ec7dcc3ed4d4385664f301
johnatanaccourtsantos <johnatan1999accourt@gmail.com>
2020-01-14 15:13:34 -0300

    Initial Commit

---
+++ js/cbf.js
@@ -0,0 +1,51 @@
+
+function camp(){
+    document.getElementById("camp").src = "img/1958_cbf.jpg";
+    document.getElementById("leftbar").style.display = "block";
+
+    document.getElementById("campanha").innerHTML = "Campanha Copa 1958";
+    document.getElementById("artilheiro").innerHTML = "Artilheiro: Pelé 6 gols";
+    document.getElementById("gols").innerHTML = "Gols Pró: 16 gols";
+    document.getElementById("local").innerHTML = "Final: Estádio Råsunda, Suécia";
+    document.getElementById("placar").innerHTML = "Brasil 5x2 Suécia";
+}
+
+function bicamp(){
+    document.getElementById("camp").src = "img/1962_cbf.jpg";
+    document.getElementById("leftbar").style.display = "block";
+    document.getElementById("campanha").innerHTML = "Campanha Copa 1962";
+    document.getElementById("artilheiro").innerHTML = "Artilheiros: Vavá e Garrincha 4 gols";
+    document.getElementById("gols").innerHTML = "Gols Pró: 14 gols";
+    document.getElementById("local").innerHTML = "Final: Estádio Nacional de Chile, Chile";
+    document.getElementById("placar").innerHTML = "Brasil 3x1 Tchecoslováquia";
+}
+
+function tricamp(){
+    document.getElementById("camp").src = "img/1970_cbf.jpg";
+    document.getElementById("leftbar").style.display = "block";
+    document.getElementById("campanha").innerHTML = "Campanha Copa 1970";
+    document.getElementById("artilheiro").innerHTML = "Artilheiro: Jairzinho 7 gols";
+    document.getElementById("gols").innerHTML = "Gols Pró: 19 gols";
+    document.getElementById("local").innerHTML = "Final: Estádio Azteca, México";
+    document.getElementById("placar").innerHTML = "Brasil 4x1 Itália";
+}
+
+function tetracamp(){
+    document.getElementById("camp").src = "img/1994_cbf.jpg";
+    document.getElementById("leftbar").style.display = "block";
+    document.getElementById("campanha").innerHTML = "Campanha Copa 1994";
+    document.getElementById("artilheiro").innerHTML = "Artilheiro: Romário 5 gols";
+    document.getElementById("gols").innerHTML = "Gols Pró: 11 gols";
+    document.getElementById("local").innerHTML = "Final: Rose Bowl, EUA";
+    document.getElementById("placar").innerHTML = "Brasil 0(3)x(2)0 Itália";
+}
+
+function pentacamp(){
+    document.getElementById("camp").src = "img/2002_cbf.jpg";
+    document.getElementById("leftbar").style.display = "block";
+    document.getElementById("campanha").innerHTML = "Campanha Copa 2002";
+    document.getElementById("artilheiro").innerHTML = "Artilheiro: Ronaldo 8 gols";
+    document.getElementById("gols").innerHTML = "Gols Pró: 18 gols";
+    document.getElementById("local").innerHTML = "Final: Estádio de Yokohama, Japão";
+    document.getElementById("placar").innerHTML = "Brasil 2x0 Alemanha";
+}
